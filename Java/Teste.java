710cd3497485ba73c478e3814a1038eb34a0d00c
JorgeOliveiraa <jorgeallancastro@live.com>
2018-05-28 11:42:03 -0300

    commit

--- Java/Teste.java
+++ Java/Teste.java
@@ -3,25 +3,51 @@
  * @author Jorge Allan de Castro Oliveira
  * @version 1 05/2018
  */
+class Pessoa {
+    private String nome;
+    private int idade;
+    private String telefone;
+
+    Pessoa() {
+        setNome(" ");
+        setIdade(0);
+        setTelefone(" ");
+    }
+
+    Pessoa(String nome, int idade, String telefone) {
+        setNome(nome);
+        setIdade(idade);
+        setTelefone(telefone);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+}
 
 public class Teste {
-    String nome;
-    int idade;
-    String telefone;
-
-    Teste() {
-        nome = "";
-        idade = 0;
-        telefone = "";
-    }
-
-    Teste(String nome, int idade, String telefone) {
-        nome = this.nome;
-        idade = this.idade;
-        telefone = this.telefone;
-    }
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Pessoa pessoa = new Pessoa("Jorge", 20, "988261885");
+        System.out.println("Olá " + pessoa.getNome() + ", você tem " + pessoa.getIdade() + " anos e seu número é: " + pessoa.getTelefone());
     }
 }
