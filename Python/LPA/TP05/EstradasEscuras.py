7efcf14e98decf5843f02bffc0e06def02a5c5d8
Jorge Oliveira <jorgeallancastro@live.com>
2018-10-06 17:22:10 -0300

    Update EstradasEscuras.py

--- Python/LPA/TP05/EstradasEscuras.py
+++ Python/LPA/TP05/EstradasEscuras.py
@@ -1,24 +1,27 @@
 import networkx as nx
 
-str = input("")
-n, m = str.split(" ")
+str = input()
+
+while str != ("0 0"):
+    n, m = str.split(" ")
         
-n = int(n)
-m = int(m)
+    n = int(n)
+    m = int(m)
     
-G = nx.Graph()
-sumG = 0
+    G = nx.Graph()
+    sumG = 0
 
-for i in range(0, m):
-    str = input("")
-    u, v, w = str.split(" ")
-    G.add_edge(int(u), int(v), weight=int(w))
-    sumG += int(w)
+    for i in range(0, m):
+        str = input("")
+        u, v, w = str.split(" ")
+        G.add_edge(int(u), int(v), weight=int(w))
+        sumG += int(w)
     
-T = nx.minimum_spanning_tree(G)
-sumT = 0
+    T = nx.minimum_spanning_tree(G)
+    sumT = 0
 
-for(u, v, wt) in T.edges.data('weight'):
-    sumT += int(wt)
+    for(u, v, wt) in T.edges.data('weight'):
+        sumT += int(wt)
 
-print(sumG - sumT)
+    print(sumG - sumT)
+    str = input()
